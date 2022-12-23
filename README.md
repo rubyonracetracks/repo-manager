04331ec1b1ca439e04a7c7c516d7974a5c57a13d
Jason Hsu <jhsu802701@jasonhsu.com>
2022-12-22 20:56:06 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# repo-manager
+# Repository Manager
+
+This repository is used for managing the repositories automatically created by Rails Neutrino.  It's also a reference on how to execute tasks with GitHub CLI.
---
+++ create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# NOTE: set -o pipefail is needed to ensure that any error or failure causes the whole pipeline to fail.
+# Without this specification, the CI status will provide a false sense of security by showing builds
+# as succeeding in spite of errors or failures.
+set -eo pipefail
+
+source definitions
+
+mkdir -p $DIR_APP
+echo '# Simple Empty Test App' > "$APP_README"
+echo 'This app was automatically created with the Repo Manager' >> "$APP_README"
