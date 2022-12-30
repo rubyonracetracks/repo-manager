5fd7e7b9f2e0c30dd44205c5c7b73c1aabddac41
Jason Hsu <jhsu802701@jasonhsu.com>
2022-12-30 15:02:18 -0600

    Added delete-test.sh and delete-test.yml

---
+++ delete-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# NOTE: set -o pipefail is needed to ensure that any error or failure causes the whole pipeline to fail.
+# Without this specification, the CI status will provide a false sense of security by showing builds
+# as succeeding in spite of errors or failures.
+set -eo pipefail
+
+source bin/definitions
+
+echo "Name of organization: $ORG_NAME"
+
+# gh api \
+  # --method POST \
+  # -H "Accept: application/vnd.github+json" \
+  # /orgs/$ORG_NAME/repos \
+  # -f name="$REPO_NAME" \
+  # -f description='Dummy test repository' \
+  # -F private=false
